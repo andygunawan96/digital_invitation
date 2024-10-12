67b28aa6f119d88c5a348da539b862c37f511f9e
Andy Gunawan <andy@rodextravel.tours>
2024-10-12 19:34:03 +0700

    refactor

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_invitation.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'template_1.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
